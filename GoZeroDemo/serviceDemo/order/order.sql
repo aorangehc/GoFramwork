8934dcb4dfb783c5624f538ce91ee11648bfbaa7
aorangehc <lier0205@163.com>
2026-01-25 16:56:10 +0800

    feat(serviceDemo):增加rpc/api/sql文件

---
+++ GoZeroDemo/serviceDemo/order/order.sql
@@ -0,0 +1,10 @@
+CREATE TABLE order (
+    order_sn VARCHAR(64) PRIMARY KEY NOT NULL COMMENT '订单号',
+    user_id BIGINT NOT NULL COMMENT '用户ID',
+    goods_id BIGINT NOT NULL COMMENT '商品ID',
+    num BIGINT NOT NULL COMMENT '数量',
+    amount BIGINT NOT NULL COMMENT '金额',
+    status INT NOT NULL COMMENT '状态',
+    create_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    delete_at DATETIME DEFAULT NULL COMMENT '删除时间'
+) ENGINE=InnoDB COLLATE utf8mb4_general_ci COMMENT='订单表';
---
+++ GoZeroDemo/serviceDemo/user/user.sql
@@ -0,0 +1,14 @@
+CREATE TABLE user (
+    id bigint AUTO_INCREMENT,
+    name varchar(255) NULL COMMENT '用户名',
+    password varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
+    mobile varchar(255) NOT NULL DEFAULT '' COMMENT '手机号码',
+    gender char(10) NOT NULL DEFAULT 'male' COMMENT '性别,male|female|unknown',
+    nickname varchar(255) NULL DEFAULT '' COMMENT '昵称',
+    type tinyint(1) NULL DEFAULT 0 COMMENT '用户类型, 0:普通用户,1:VIP用户,用于测试golang关键字',
+    create_at timestamp NULL,
+    update_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE mobile_index (mobile),
+    UNIQUE name_index (name),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB COLLATE utf8mb4_general_ci COMMENT '用户表';
