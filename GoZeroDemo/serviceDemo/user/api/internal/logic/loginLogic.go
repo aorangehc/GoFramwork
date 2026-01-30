d7877cb13c6de66e6327b53501c489688f1edfaa
aorangehc <lier0205@163.com>
2026-01-30 22:27:31 +0800

    feat(serviceDemo):实现简单登录和用户信息获取功能

--- GoZeroDemo/serviceDemo/user/api/internal/logic/loginLogic.go
+++ GoZeroDemo/serviceDemo/user/api/internal/logic/loginLogic.go
@@ -5,6 +5,10 @@ package logic
 
 import (
 	"context"
+	"crypto/md5"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
 
 	"gozeroservicedemo/user/api/internal/svc"
 	"gozeroservicedemo/user/api/internal/types"
@@ -27,7 +31,27 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginResp, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	//先检查用户和密码是否为空
+	if req.Password == "" || req.UserName == "" {
+		return nil, fmt.Errorf("用户名或密码不能为空")
+	}
+	// 现在检查正确性
+	// 检查用户并取出密码
+	user, err := l.svcCtx.UserModel.FindOneByName(l.ctx, sql.NullString{String: req.UserName, Valid: true})
+	if err != nil {
+		logx.Errorw("user_Login_UserModel.FindOneByName error", logx.Field("err", err))
+		return nil, fmt.Errorf("用户不存在")
+	}
+	// 检查密码是否正确
+	h := md5.New()
+	h.Write([]byte(req.Password))
+	h.Write(secret)
+	passwordStr := hex.EncodeToString(h.Sum(nil))
+	if user.Password != passwordStr {
+		logx.Infof("user_Login_password not match, userId: %d", user.Id)
+		return nil, fmt.Errorf("密码错误")
+	}
+	return &types.LoginResp{
+		Token: "114514",
+	}, nil
 }
--- GoZeroDemo/serviceDemo/user/api/internal/logic/userInfoLogic.go
+++ GoZeroDemo/serviceDemo/user/api/internal/logic/userInfoLogic.go
@@ -5,11 +5,13 @@ package logic
 
 import (
 	"context"
+	"fmt"
 
 	"gozeroservicedemo/user/api/internal/svc"
 	"gozeroservicedemo/user/api/internal/types"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type UserInfoLogic struct {
@@ -27,7 +29,33 @@ func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfo
 }
 
 func (l *UserInfoLogic) UserInfo(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
-	// todo: add your logic here and delete this line
+	// 参数校验
+	if req.Id == 0 || req.Token == 0 {
+		return nil, fmt.Errorf("参数错误")
+	}
+
+	// 获取数据
+	user, err := l.svcCtx.UserModel.FindOne(l.ctx, req.Id)
+	if err != nil && err != sqlx.ErrNotFound {
+		logx.Errorw("user_UserInfo_UserModel.FindOne error", logx.Field("err", err))
+		return nil, fmt.Errorf("错误: %v", err)
+	}
+	if err == sqlx.ErrNotFound {
+		logx.Info("user_UserInfo_UserModel.FindOne not found", logx.Field("userId", req.Id))
+		return nil, fmt.Errorf("用户不存在")
+	}
+
+	// 获取成功
+	resp = &types.UserInfoResp{
+		Id:       user.Id,
+		UserName: user.Name.String,
+		Gender:   int(user.Gender),
+		Mobile:   user.Mobile,
+		Token:    "114514",
+		NickName: user.Nickname,
+		CreateAt: user.CreateAt.Time.String(),
+		UpdateAt: user.UpdateAt.String(),
+	}
 
 	return
 }
